2598e14bc016d44fdf94787e660484acbf77c63d
Jenny <sacxjenny@live.com>
2017-02-02 12:42:04 -0700

    Made a skeleton of what I want to try and implement

---
+++ ClockRunner.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class ClockRunner{
+	
+	public static void main(String[] args){
+
+		// this area calls functions and stuff
+
+		// get time
+		// check ring 
+
+		AlarmClock mainClock = new AlarmClock();
+		//mainClock.checkTime();
+		//mainClock.checkAlarm();
+
+	}
+
+}
+
+public class AlarmClock{
+
+	private int second;
+	private int minute;
+	private int hour;
+	
+	// constructor
+
+	AlarmClock(){
+
+	}
+
+	// get the time
+	// this returns void, but will eventually return int
+
+	public static void checkTime(){
+
+	}
+
+
+	// check the alarm
+	// see if it rings or not
+
+	public static void checkAlarm(){
+
+	}
+
+
+
+}
