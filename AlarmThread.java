e5c55bbe476e628d78bb2bcc7656c85206fa1066
Carguy18 <aaronkobelsky@gmail.com>
2017-02-06 12:09:25 -0700

    Updated Alarm Thread To Use Jenny's Code

--- AlarmThread.java
+++ AlarmThread.java
@@ -2,18 +2,18 @@
 public class AlarmThread extends Thread{
 	
 	public boolean terminate = false;
-	//public JennyAlarm alarm;
+	public AlarmClock alarm;
 	
 	public void run(){
 		while(!terminate){
 			//call function to check if the alarm should ring
+			alarm.checkAlarm();
 			
 			//wait about 0.1 seconds
 			try {
 				currentThread().wait(10);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				//Ignore
 			}
 		}
 	}
