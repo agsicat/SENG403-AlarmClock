4951ba649b2009f8d88184064512d29bba46d616
Carguy18 <aaronkobelsky@gmail.com>
2017-03-09 14:02:53 -0700

    BugFix: Single digit times are displayed as 2 digits (1 -> 01)

--- Gui.java
+++ Gui.java
@@ -5,7 +5,7 @@
  *
  * @author Francisco Garcia
  * @Edit Aaron Kobelsky
- * @version 2.2
+ * @version 3.2
  */
 
 import java.awt.*;
@@ -25,8 +25,7 @@ public class Gui extends JFrame implements ActionListener, Runnable{
     private boolean doAnalogDisplay = false;
 
     //storage for the alarms in the system
-    private static threadSpawner alarms = new threadSpawner();
-    private long alarmID;
+    public static threadSpawner alarms = new threadSpawner();
 
     /**
      * Constructor
@@ -96,12 +95,28 @@ public class Gui extends JFrame implements ActionListener, Runnable{
 
 
         String temp;
+        String tempminute = "";
+        String tempsecond = "";
+        
+        if(10 > (int)time.get(Calendar.MINUTE)){
+        	tempminute = "0"+time.get(Calendar.MINUTE);
+        }
+        else{
+        	tempminute += time.get(Calendar.MINUTE);
+        }
+        
+        if(10 > (int)time.get(Calendar.SECOND)){
+        	tempsecond = "0"+time.get(Calendar.SECOND);
+        }
+        else{
+        	tempsecond += time.get(Calendar.SECOND);
+        }
 
         if(time.get(Calendar.HOUR) == 0){
-            temp =  12 + ":" + time.get(Calendar.MINUTE) + ":" + time.get(Calendar.SECOND) + " " + AMPM;
+            temp =  12 + ":" + tempminute + ":" + tempsecond + " " + AMPM;
         }
         else {
-            temp =  time.get(Calendar.HOUR) + ":" + time.get(Calendar.MINUTE) + ":" + time.get(Calendar.SECOND) + " " + AMPM;
+            temp =  time.get(Calendar.HOUR) + ":" + tempminute + ":" + tempsecond + " " + AMPM;
         }
 
         return temp;
@@ -167,9 +182,6 @@ public class Gui extends JFrame implements ActionListener, Runnable{
             JButton cancelBtn = new JButton("Cancel");
             btn.addActionListener(this);
             cancelBtn.addActionListener(this);
-            
-            JButton rbtn = new JButton("Dismiss");
-            rbtn.addActionListener(this);
 
             Container cont = frame.getContentPane();
             cont.setLayout(new FlowLayout());
@@ -181,7 +193,6 @@ public class Gui extends JFrame implements ActionListener, Runnable{
             cont.add(time);
 
             cont.add(btn);
-            cont.add(rbtn);
             cont.add(cancelBtn);
         }
 
@@ -193,18 +204,13 @@ public class Gui extends JFrame implements ActionListener, Runnable{
             	a.setInputHour(date.getHours());
             	a.setInputMinute(date.getMinutes());
             	a.setAlarmSet(true);
-            	alarmID = alarms.spawnNewThread(a);
+            	alarms.spawnNewThread(a);
                 JOptionPane.showMessageDialog(null, "Alarm set for: "+date.getHours() +":"+ date.getMinutes());
             }
 
             else if (temp == "Cancel") {
-                alarms.cancelAlarm(alarmID);
+                //alarms.cancelAlarm(alarmID);
                 JOptionPane.showMessageDialog(null, "Alarm Cancelled");
-            }
-            
-            else if (temp == "Dismiss"){
-            		alarms.dismissAlarm(alarmID);
-                    JOptionPane.showMessageDialog(null, "Alarm Dismissed");
             }
         }
 
