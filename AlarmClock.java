de7a06c1b79a6ff00796f4c7fd6469aa09e1d262
Carguy18 <aaronkobelsky@gmail.com>
2017-04-09 21:03:00 -0600

    Complete Refactor of AlarmClock to make it work better with everything else

--- AlarmClock.java
+++ AlarmClock.java
@@ -1,9 +1,11 @@
+package alarmClockThreads;
 import java.util.*;
+
+import alarmGUIs.DismissAlarm;
+
 import java.time.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.LocalDateTime;
-import java.time.Month;
 
 /**
  * Object which acts as an alarm clock.
@@ -13,24 +15,22 @@ import java.time.Month;
  * @author Jenny Le
  * @edit Aaron Kobelsky, Gave each alarm a unique ID number
  * @edit Aaron Kobelsky, Implemented alarms being given labels
- * @version 3.1
+ * @Edit Aaron Kobelsky, Removed dead code and implemented weekly repeating alarms being set for multiple days
+ * @version 3.2
  */
 public class AlarmClock{
 
-	private LocalDate currentDate;
-	private LocalTime currentTime;
+	//hour and minute at which the alarm is set to ring
+	private int alarmHour;
+	private int alarmMinute;
+	//days for which a weekly repaeting alarm will ring
+	private HashMap<Integer, Boolean> alarmDays;
 	
-	private DismissAlarm d;
-	
-	private Date alarmTime;
-	
-	private int inputHour;
-	private int inputMinute;
-	private int inputDay;
-	
+	//booleans for error checking
 	private boolean alarmSet = false;
 	private boolean checkRing = false;
 	
+	//booleans to represent when an alarm should repeat daily or weekly
 	private boolean repeatDaily = false;
 	private boolean repeatWeekly = false;
 	
@@ -39,80 +39,105 @@ public class AlarmClock{
 	
 	//label to identify each alarm
 	private String alarmLabel;
+	
+	//flag used to manage dismissal of repeating alarms
+	private boolean repeatDismiss = false;
 
-	// constructor
-	public AlarmClock(Date d){
-		alarmTime = d;
-		currentDate = LocalDate.of(2017,1,1);
-		currentTime = LocalTime.of(0, 0); 
-		
+	/**
+	 * Constructor for the alarm class
+	 * Any method calling this constructor must follow the following data constraints
+	 * @param hour integer between 0-23 representing the hour at which the alarm will ring
+	 * @param min integer between 0-59 representing the minute at which the alarm will ring
+	 * @param days HashMap<Integer, Boolean> containing keys 1-7, the boolean associated with each indicating whether the alarm should ring that day of the week
+	 * @param daily boolean to indicate whether the alarm should repeat daily
+	 * @param weekly boolean to indicate whether the alarm should repeat weekly
+	 * No alarm can be both daily and weekly repeating
+	 * @param label String to serve as a label for the alarm
+	 */
+	public AlarmClock(int hour, int min, HashMap<Integer, Boolean> days, boolean daily, boolean weekly, String label){
 		//Initialize the ID to be the unique HashCode of this object
 		alarmID = System.identityHashCode(this);
 		
-		alarmLabel = "Alarm";
-	}
-
-
-	public LocalTime checkTime(){
-		currentTime = LocalTime.now();
-		System.out.println("Current time: " + currentTime);
-		return currentTime;
+		//initialize the alarm variables
+		alarmLabel = label;
+		alarmHour = hour;
+		alarmMinute = min;
+		alarmDays = days;
+		repeatDaily = daily;
+		repeatWeekly = weekly;
 	}
 
 	// check the alarm
 	// see if it rings or not
-
 	public boolean checkAlarm(){
-		if(!checkRing){
-			if(alarmSet == false){
-				//this should be replaced by an exception in later versions
-				System.out.println("An alarm has not been set");
-				return checkRing;
-			} 
-			
-			currentTime = LocalTime.now(); // update and get current time 
-			currentDate = LocalDate.now();
+		if(!repeatDismiss){
+			if(!checkRing){
+				if(alarmSet == false){
+					//this should be replaced by an exception in later versions
+					System.out.println("An alarm has not been set");
+					return checkRing;
+				} 
+				
+				LocalTime currentTime = LocalTime.now(); // update and get current time 
+				LocalDate currentDate = LocalDate.now();
+				int hour = currentTime.getHour();
+				int minute = currentTime.getMinute();
+				int day = 0;
+				//get the current day as an int
+				if(currentDate.getDayOfWeek() == DayOfWeek.MONDAY){
+					day = 1;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.TUESDAY){
+					day = 2;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.WEDNESDAY){
+					day = 3;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.THURSDAY){
+					day = 4;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.FRIDAY){
+					day = 5;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.SATURDAY){
+					day = 6;
+				}
+				else if(currentDate.getDayOfWeek() == DayOfWeek.SUNDAY){
+					day = 7;
+				}
+				//error check
+				if(day == 0){
+					return false;
+				}
+				
+				// check if hour and minute is equal to alarm
+				if(!repeatDaily){
+					if ((hour == alarmHour) && (minute == alarmMinute) && (alarmDays.containsKey(day) && alarmDays.get(day))){
+						checkRing = true;
+						//spawn the ringing alarm gui
+						new DismissAlarm(alarmID);
+						
+						return checkRing;
+					} 
+				}
+				else{
+					if ((hour == alarmHour) && (minute == alarmMinute)){
+						checkRing = true;
+						//spawn the ringing alarm gui
+						new DismissAlarm(alarmID);
+						
+						return checkRing;
+					} 
+				}
+			}
+		}
+		else{
+			LocalTime currentTime = LocalTime.now(); // update and get current time 
 			int hour = currentTime.getHour();
 			int minute = currentTime.getMinute();
-			int day = 0;
-			if(currentDate.getDayOfWeek() == DayOfWeek.MONDAY){
-				day = 1;
+			if ((hour != alarmHour) || (minute != alarmMinute)){
+				repeatDismiss = false;
 			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.TUESDAY){
-				day = 2;
-			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.WEDNESDAY){
-				day = 3;
-			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.THURSDAY){
-				day = 4;
-			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.FRIDAY){
-				day = 5;
-			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.SATURDAY){
-				day = 6;
-			}
-			else if(currentDate.getDayOfWeek() == DayOfWeek.SUNDAY){
-				day = 7;
-			}
-			//error check
-			if(day == 0){
-				return false;
-			}
-			
-			// check if hour and minute is equal to alarm
-			if ((hour == inputHour) && (minute == inputMinute) && (day == inputDay)){
-				checkRing = true;
-				//spawn the ringing alarm gui, in future will play ringtone as well
-				d = new DismissAlarm(alarmID);
-				
-				//System.out.println("Ring ring ring! Alarm going off! It is " + hour + ":" + minute);
-				return checkRing;
-			} 
-			//do nothing
-			//System.out.println("It is not time to ring");
-			return checkRing;
 		}
 		return checkRing;
 	}
@@ -129,45 +154,21 @@ public class AlarmClock{
 	public void setAlarmSet(boolean b){
 		this.alarmSet = b;
 	}
-	
-	public void setInputHour(int h){
-		this.inputHour = h;
+
+	public int getAlarmHour(){
+		return this.alarmHour;
 	}
 
-	public int getInputHour(){
-		return this.inputHour;
-	}
-
-	public int getInputMinute(){
-		return this.inputMinute;
-	}
-	
-	public void setInputMinute(int m){
-		this.inputMinute = m;
+	public int getAlarmMinute(){
+		return this.alarmMinute;
 	}
 	
 	public long getAlarmID(){
 		return this.alarmID;
 	}
 	
-	public void setAlarmLabel(String label){
-		this.alarmLabel = label;
-	}
-	
 	public String getAlarmLabel(){
 		return this.alarmLabel;
-	}
-	
-	public Date getAlarmDate(){
-		return this.alarmTime;
-	}
-	
-	public void setRepeatDaily(boolean D){
-		repeatDaily = D;
-	}
-	
-	public void setRepeatWeekly(boolean W){
-		repeatWeekly = W;
 	}
 	
 	public boolean getRepeatDaily(){
@@ -178,12 +179,12 @@ public class AlarmClock{
 		return this.repeatWeekly;
 	}
 	
-	public void setInputDay(int D){
-		inputDay = D;
+	public HashMap<Integer, Boolean> getAlarmDays(){
+		return this.alarmDays;
 	}
 	
-	public int getInputDay(){
-		return this.inputDay;
+	public void setRepeatDismiss(boolean b){
+		this.repeatDismiss = b;
 	}
 
 }
